b30ad80b9e423579ffd8eebf2234c7c9774e0be9
haqim007 <32091226+haqim007@users.noreply.github.com>
2024-08-05 16:02:43 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Quipper Indonesia on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
